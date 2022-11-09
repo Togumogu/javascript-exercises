c6336b0c42d093570c802c249a7b687c6cf7e8b8
Togumogu <toygun.baysal@hotmail.com>
2022-11-09 13:39:28 +0300

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR!!!"
+    }
+    let phrase = ""
+    for (let i = 1; i <= num; i++) {
+        phrase += str + " "
+    } 
+    return phrase
 };
+
+console.log(repeatString("Toygun", 7))
+
 
 // Do not edit below this line
 module.exports = repeatString;
