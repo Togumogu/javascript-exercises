777009551282101dc50a530a3c678c7da87d237d
Togumogu <toygun.baysal@hotmail.com>
2022-11-10 21:28:40 +0300

    I actually got very close with a different solution. It works with strings but with integers cant get rid of quotation marks.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,12 @@
 const removeFromArray = function(array, remove) {
     let strArray = array.toString()
-    strArray = strArray.replace(remove, "")
+    strArray = strArray.replace(remove + ",", "")
     console.log(strArray)
-    let joinedArray = strArray.split(" ")
+    let joinedArray = strArray.split(",")
     return joinedArray
 };
 
-console.log(removeFromArray([1, 2, 3, 4], 3))
+console.log(removeFromArray([1, 2, 3, 4], 3,))
 
 // Do not edit below this line
 module.exports = removeFromArray;
