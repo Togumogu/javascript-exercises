c13b78dec347e88ef755e8ca276d94577474a6d7
Togumogu <toygun.baysal@hotmail.com>
2022-11-10 20:11:47 +0300

    Probably going to use splice and a loop. Trying to figure out how.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let word = ""
+    
+    
+    let word = str.slice(str.length-1)
+    return word
 };
+
+console.log(reverseString("Hello"))
 
 // Do not edit below this line
 module.exports = reverseString;
