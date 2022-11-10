b2c6ec4433530c622c41c73af8233dbd6ca3882d
Togumogu <toygun.baysal@hotmail.com>
2022-11-10 20:53:46 +0300

    Made substantial progress, although I have holes in my array. If I can solve that, I am good.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, remove) {
+    let strArray = array.toString()
+    strArray = strArray.replace(remove, "")
+    console.log(strArray)
+    let joinedArray = strArray.split(" ")
+    return joinedArray
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
