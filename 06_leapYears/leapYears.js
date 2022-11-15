d6a8d7b74a41f4ac456b43dfe362f42119743aef
Togumogu <toygun.baysal@hotmail.com>
2022-11-15 21:11:22 +0300

    Completed leap years, was not that hard, mostly mathematical.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,14 +2,14 @@ const leapYears = function(year) {
     
     if (isNaN(year) === true ) return "ERROR!!!"
 
-    if (year % 4 === 0 && year % 400 === 0 && year % 100 === !0) {
+    if (year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0)) {
         return true
     } else {
         return false
     }
 }
 
-console.log(leapYears(1600))
+console.log(leapYears(600))
 
 
 // Do not edit below this line
