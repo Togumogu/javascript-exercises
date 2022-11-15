c6ea739822ae2b1af31cd16f76379c67d7dd07c5
Togumogu <toygun.baysal@hotmail.com>
2022-11-15 21:30:45 +0300

    Initial commit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
+const ftoc = function(fah) {
 
+    let celcius = ((fah - 32)*5)/9
+    return Math.floor(celcius * 10)/10
+    
 };
+console.log(ftoc(100))
 
-const ctof = function() {
-
-};
-
-// Do not edit below this line
-module.exports = {
-  ftoc,
-  ctof
-};
+const ctof = function(cel) {
+    let fahren = (cel * 9/5 + 32)
+    return Math.floor(fahren * 10)/10
+}
+console.log(ctof(-10))
