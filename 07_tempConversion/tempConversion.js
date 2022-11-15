b59cc97c6ed1359da54dedef230f9933a23f3c52
Togumogu <toygun.baysal@hotmail.com>
2022-11-15 21:32:34 +0300

    Used math floor instead of round, corrected it

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
 const ftoc = function(fah) {
 
     let celcius = ((fah - 32)*5)/9
-    return Math.floor(celcius * 10)/10
+    return Math.round(celcius * 10)/10
     
 };
 console.log(ftoc(100))
 
 const ctof = function(cel) {
     let fahren = (cel * 9/5 + 32)
-    return Math.floor(fahren * 10)/10
+    return Math.round(fahren * 10)/10
 }
-console.log(ctof(-10))
+console.log(ctof(73.2))
