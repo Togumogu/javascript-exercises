fa1d344387c5c1970bd94c41027063aa2e16e839
Togumogu <toygun.baysal@hotmail.com>
2022-11-15 20:00:18 +0300

    Solved the bigger value, samller value problem. However think I did this in a more comvoluted way, will take a look at the answers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    // Error code, doesn't accept negative or non-number values.
+    if (num1 < 0 || num2 < 0 || isNaN(num1) === true || isNaN(num2) === true) {
+        return "ERROR!!!"
+    }
+
+   // Give the numbers values for the loop
+   let biggerNumber = 0
+   let smallerNumber = 0
+   smallerNumber = num1
+   biggerNumber = num2
+
+   // If the first number is bigger, reverse the values.
+   if (num1 > num2) {
+        smallerNumber = num2
+        biggerNumber = num1
+   }
+
+    // For loop, sums the numbers
+    let numbers = 0
+    for (i = smallerNumber; i <= biggerNumber; i++) {
+        numbers += i
+    }
+    return numbers
 };
+
+console.log(sumAll(123, 1))
 
 // Do not edit below this line
 module.exports = sumAll;
